e9c8ad18bb40675610c1aab454068b7e12644a89
rizmaksol <rizwan.muhammadak@gmail.com>
2026-06-03 00:54:29 +0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -8,7 +8,8 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getAuth }        from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 import { getFirestore }   from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
-import { getStorage }     from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
+// import { getStorage }
+// from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
 
 // ── REPLACE THESE WITH YOUR REAL VALUES ──────────────────────
 const firebaseConfig = {
@@ -26,4 +27,4 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db   = getFirestore(app);
-export const storage = getStorage(app);
+// export const storage = getStorage(app);;
